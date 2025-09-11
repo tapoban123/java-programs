e1330af65f985160e94ec371f2ad6cace6029375
Tapoban Ray <raytapoban@gmail.com>
2025-09-11 15:22:02 +0530

    Implemented Shape class

--- Assignment_2/Qs_2/Driver.java
+++ Assignment_2/Qs_2/Driver.java
@@ -1,6 +1,34 @@
 package Qs_2;
 
+import java.util.Scanner;
+
 class Driver {
     public static void main(String[] args) {
+        Scanner inputObj = new Scanner(System.in);
+        Shape shapeObj = new Shape();
+
+        System.out.println("Calculating area of Square");
+        System.out.print("Enter value of side: ");
+        float side = inputObj.nextFloat();
+        float squareArea = shapeObj.calcArea(side);
+        System.out.println("Area of Square = " + squareArea);
+
+        System.out.println("\nCalculating area of Rectangle");
+        System.out.print("Enter length: ");
+        int length = inputObj.nextInt();
+        System.out.print("Enter breadth: ");
+        int breadth = inputObj.nextInt();
+        int rectArea = shapeObj.calcArea(length, breadth);
+        System.out.println("Area of Rectangle = " + rectArea);
+
+        System.out.println("\nCalculating area of Triangle");
+        System.out.print("Enter base: ");
+        float base = inputObj.nextFloat();
+        System.out.print("Enter height: ");
+        float height = inputObj.nextFloat();
+        float triangleArea = shapeObj.calcArea(base, height);
+        System.out.println("Area of Triangle = " + triangleArea);
+
+        inputObj.close();
     }
 }
--- Assignment_2/Qs_2/Shape.java
+++ Assignment_2/Qs_2/Shape.java
@@ -1,5 +1,15 @@
 package Qs_2;
 
 public class Shape {
+    public float calcArea(float side) {
+        return side * side;
+    }
 
+    public int calcArea(int length, int breadth) {
+        return length * breadth;
+    }
+
+    public float calcArea(float base, float height) {
+        return (base * height) / 2;
+    }
 }
